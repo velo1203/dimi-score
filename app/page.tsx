168188776f3c47688ffb67be66cf5334f7f1fba2
velo1203 <hokitoki1203@gmail.com>
2025-04-06 20:18:40 +0900

    font edit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import styles from "./page.module.css";
 import { createClient } from "@supabase/supabase-js";
 
@@ -14,6 +15,13 @@ export default async function Home() {
 
     return (
         <div className={styles.page}>
+            <Head>
+                <link
+                    rel="stylesheet"
+                    as="style"
+                    href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css"
+                />
+            </Head>
             <div className={`${styles.half} ${styles.blue}`}>
                 <div className={styles.score}>{blue?.score ?? 0}</div>
             </div>
