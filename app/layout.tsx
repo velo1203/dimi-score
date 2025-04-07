14c9f8b625a3eefd57c2cf881dfb456a9a69201a
velo1203 <hokitoki1203@gmail.com>
2025-04-07 18:11:36 +0900

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,17 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-    title: "score",
-    description: "스코어보두",
+    title: "스코어보드",
+    description: "스코어보드",
+
+    openGraph: {
+        title: "스코어보드",
+        description: "디미고 스코어보드",
+        url: "https://score.devho.net",
+        siteName: "score",
+        locale: "ko_KR",
+        type: "website",
+    },
 };
 
 export default function RootLayout({
