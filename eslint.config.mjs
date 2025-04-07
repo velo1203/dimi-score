fdc48c012918483c8ede054468309a9e1bf8c046
velo1203 <hokitoki1203@gmail.com>
2025-04-07 18:21:29 +0900

    add

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,13 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
     ...compat.extends("next/core-web-vitals", "next/typescript"),
-
-    {
-        files: ["**/*.ts", "**/*.tsx"],
-        rules: {
-            "@typescript-eslint/no-explicit-any": "off",
-        },
-    },
 ];
 
 export default eslintConfig;
